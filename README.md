7c6ac28684ed0b4abef3cb2facb5096f12dcf971
igor <igor_miazek@protonmail.com>
2020-06-24 14:35:36 +0200

    feat: Pre processing data. Getting list of observations for each ship id which meet the project requirements, namely 2 observations which timedelta is around 30 minutes. Resolves: TASK

--- README.md
+++ README.md
@@ -25,6 +25,23 @@ Because we are not so skilled in R yet, we will use Python to pre process
 data and store it into simple database that could be used when app will be
 deployed for example sqlite.
 
+During pre processing data we encounter an issue with distance calcualtion.
+So first we need to provide a function to calcuate dinstance between 2 points
+and keep the highest distance.
+
+At the moment we have created an approach where we iterate through each 
+ship id dataset and compare all observations, so from each ship dataset we need to get a list of observations for which timedelta is around 30 minutes. If we would like
+to do it in advance it may take some time so I wonder if it is not better to
+calculate it on the fly. The possible solution would be usage of some time 
+series database which could efficently filter observations by timedelta ?
+
+For getting timedelta between observations I am thinking as well about a 2d matrix
+to get a set of all possible relationships between observations and than run some
+efficient operation on matrix.
+
+Part of the work was done on jupyternotebook which will be attached separately
+for confidential reasons as it extract information about dataset.
+
 ### 1. Create generic interface to read data.
 Here we will try to use best techniques we have found so far.
 
--- pre_process_data.py
+++ pre_process_data.py
@@ -2,48 +2,110 @@ import pandas as pd
 import sqlalchemy
 from collections import Counter
 
+
 def create_database():
-  pass
-  
+    pass
+
+
 def create_schema_and_models():
-  pass
-  
+    pass
+
+
 def insert_data():
-  pass
+    pass
+
 
 ALL_COLUMNS = ['LAT', 'LON', 'SPEED', 'COURSE', 'HEADING', 'ELAPSED', 'DESTINATION',
                'FLAG', 'LENGTH', 'ROT', 'SHIPNAME', 'SHIPTYPE', 'SHIP_ID', 'WIDTH',
                'L_FORE', 'W_LEFT', 'DWT', 'GT_SHIPTYPE', 'LEGEND', 'DATETIME', 'PORT',
                'date', 'week_nb', 'ship_type', 'port', 'is_parked']
 
-COLUMNS = ['LAT', 'LON', 'DESTINATION', 'SHIPNAME', 'SHIPTYPE', 'SHIP_ID', 
+COLUMNS = ['LAT', 'LON', 'DESTINATION', 'SHIPNAME', 'SHIPTYPE', 'SHIP_ID',
            'DATETIME', 'date', 'ship_type']
-def main():
-  df = pd.read_csv('./ships_data/ships.csv', usecols=ALL_COLUMNS)
-  df_g_by_sid_and_sname = df.groupby(["SHIP_ID", "SHIPNAME"]).agg({'SHIP_ID': ['count']})
-  n_of_idx_per_ship_id = Counter([idx[0] for idx in df_g_by_sid_and_sname.index.values]).most_common()
 
-  list_of_ship_id_to_exclude = []
-  for x in n_of_idx_per_ship_id:
-      
-      if x[1] > 1:
-          counter += 1
-          list_of_ship_id_to_exclude.append(x[0])
-  # we exclude ship ids which are related with more than 1 ship names as we think
-  # that this is breaks relationship in data
-  df = df[~df["SHIP_ID"].isin(list_of_ship_id_to_exclude)]
-  # the correctness of this approach was already proved in jupyternotebook
-  return df
-  
+
+def main():
+    df = pd.read_csv('./ships_data/ships.csv', usecols=COLUMNS)
+    df_g_by_sid_and_sname = df.groupby(["SHIP_ID", "SHIPNAME"]).agg({'SHIP_ID': ['count']})
+    n_of_idx_per_ship_id = Counter([idx[0] for idx in df_g_by_sid_and_sname.index.values]).most_common()
+
+    list_of_ship_id_to_exclude = []
+    for x in n_of_idx_per_ship_id:
+
+        if x[1] > 1:
+            list_of_ship_id_to_exclude.append(x[0])
+    # we exclude ship ids which are related with more than 1 ship names as we think
+    # that this is breaks relationship in data
+    df = df[~df["SHIP_ID"].isin(list_of_ship_id_to_exclude)]
+    # the correctness of this approach was already proved in jupyternotebook
+
+    # convert DATETIME
+    df = df.astype({'DATETIME': 'datetime64'})
+    return df
+
 
 def get_ship_types(df):
-  pass
-  
+    """
+    Return data frame of unique ship types
+    :param df:
+    :return:
+    """
+    return df.groupby(['SHIPTYPE', 'ship_type'], as_index=False).size()
+
+
 def get_ships_names(df):
-  pass
-  
+    """
+    Return data frame of unique ship names
+    :param df:
+    :return:
+    """
+    return df.groupby(['SHIP_ID', 'SHIPNAME'], as_index=False).size()
+
+
+RECORD_COLUMNS = ['LAT', 'LON', 'DESTINATION', 'SHIPTYPE', 'SHIP_ID',  'DATETIME']
+
+
 def get_records(df):
-  pass
+    """
+    Select only relevant columns and sort by date.
+    :param df:
+    :return:
+    """
+    df = df[RECORD_COLUMNS]
+    return df
+
+
+def __get_distance(x, y):
+    pass
+
+
+def get_observations(df, ships):
+    """
+    Here we plan to loop over each ship id dataset and find observations between which
+    timedelta is around 30 minutes. Next we need to select longest one and save it.
+    :param df:
+    :param ships: list
+    :return:
+    """
+    compare_lower_bound = pd.Timedelta(29, unit='m')
+    compare_higher_bound = pd.Timedelta(31, unit='m')
+
+    for ship_id in ships:
+        print('Getting observations for {0}.\n'.format(ship_id))
+        __df = df[(df.SHIP_ID == ship_id)]
+        for idx, row in __df.iterrows():
+            row = row
+            __time_delta_mask = (compare_lower_bound <= (__df.DATETIME - row[6])) & \
+                                ((__df.DATETIME - row[6]) <= compare_higher_bound)
+            __row_idenity_mask = (__df.index == idx)
+            __resulting_df = __df[__time_delta_mask | __row_idenity_mask]
+    return
+
 
 if __name__ == "__main__":
-  df = main()
+    df = main()
+    df_ship_types = get_ship_types(df)
+    df_ship_names = get_ships_names(df)
+    df_records = get_records(df)
+    df_observations = get_observations(df, map(lambda x: x[0], df_ship_names.index.values))
+    pass
